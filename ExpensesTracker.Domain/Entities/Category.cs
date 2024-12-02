3464a85bdde4160969814186c67adc179007483c
Antunes <36316038+zpdh@users.noreply.github.com>
2024-12-02 05:21:19 -0300

    feat: add initial migration

--- ExpensesTracker.Domain/Entities/Category.cs
+++ ExpensesTracker.Domain/Entities/Category.cs
@@ -3,5 +3,6 @@
 public class Category
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public IEnumerable<Expense> Expenses { get; set; } = default!;
 }
---
+++ ExpensesTracker.Infrastructure/Dependencies.cs
@@ -0,0 +1,24 @@
+﻿using ExpensesTracker.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExpensesTracker.Infrastructure;
+
+public static class Dependencies
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDatabase(configuration);
+    }
+
+    private static void AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<DataContext>(options => {
+            var connectionString = configuration.GetConnectionString("MySql");
+            var version = ServerVersion.AutoDetect(connectionString);
+
+            options.UseMySql(connectionString, version);
+        });
+    }
+}
