1f0689a7db82267a0f5f30868f296905f2fb0bcd
Antunes <36316038+zpdh@users.noreply.github.com>
2024-12-02 04:22:14 -0300

    feat: add initial entities

--- ExpensesTracker.Domain/Entities/Expense.cs
+++ ExpensesTracker.Domain/Entities/Expense.cs
@@ -2,5 +2,10 @@
 
 public class Expense
 {
-    
+    public int Id { get; set; }
+    public int CategoryId { get; set; }
+    public int UserId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public double Price { get; set; }
+    public DateTime InsertionDate { get; set; } = DateTime.UtcNow;
 }
