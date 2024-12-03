e62fb486d61f84f9c96a354ba5ecaaacfe874a2e
Antunes <36316038+zpdh@users.noreply.github.com>
2024-12-03 12:56:26 -0300

    feat: add global exception handler

---
+++ ExpensesTracker.Api/ExceptionHandler/GlobalExceptionHandler.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpensesTracker.Api.ExceptionHandler;
+
+public class GlobalExceptionHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+            httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            await httpContext.Response.WriteAsJsonAsync(new ProblemDetails
+                {
+                    Type = exception.GetType().Name,
+                    Title = "An unknown error occurred while processing your request.",
+                    Detail = exception.Message
+                },
+                cancellationToken);
+
+            return true;
+    }
+}
--- ExpensesTracker.Api/Program.cs
+++ ExpensesTracker.Api/Program.cs
@@ -1,10 +1,11 @@
+using ExpensesTracker.Api.ExceptionHandler;
 using ExpensesTracker.Application;
 using ExpensesTracker.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -24,6 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseExceptionHandler();
 
 app.UseHttpsRedirection();
 
