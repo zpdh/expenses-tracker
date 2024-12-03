d5c4193ecd2365fe0fc9819af794a882c1e49b3e
Antunes <36316038+zpdh@users.noreply.github.com>
2024-12-03 13:07:46 -0300

    feat: add classes to represent ValidationPipelineBehavior.cs results

---
+++ ExpensesTracker.Domain/Results/IValidationResult.cs
@@ -0,0 +1,9 @@
+﻿using ExpensesTracker.Domain.Errors.Base;
+
+namespace ExpensesTracker.Domain.Results;
+
+public interface IValidationResult
+{
+    public static readonly Error ValidationError = new("A validation problem occurred.");
+    ICollection<Error> Errors { get; }
+}
---
+++ ExpensesTracker.Domain/Results/ValidationResult.cs
@@ -0,0 +1,26 @@
+﻿using ExpensesTracker.Domain.Errors.Base;
+
+namespace ExpensesTracker.Domain.Results;
+public class ValidationResult : Result, IValidationResult
+{
+    public ICollection<Error> Errors { get; }
+
+    private ValidationResult(ICollection<Error> errors) : base(IValidationResult.ValidationError)
+    {
+        Errors = errors;
+    }
+
+    public static ValidationResult WithErrors(ICollection<Error> errors) => new(errors);
+}
+
+public class ValidationResult<TValue> : Result<TValue>, IValidationResult
+{
+    public ICollection<Error> Errors { get; }
+
+    private ValidationResult(ICollection<Error> errors) : base(default, IValidationResult.ValidationError)
+    {
+        Errors = errors;
+    }
+
+    public static ValidationResult<TValue> WithErrors(ICollection<Error> errors) => new(errors);
+}
