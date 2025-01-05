08770c3c60fe2886bf46cf17c9969dbff8c6b635
Antunes <36316038+zpdh@users.noreply.github.com>
2025-01-04 21:14:35 -0300

    chore: document Category.cs

--- src/ExpensesTracker.Domain/Entities/Category.cs
+++ src/ExpensesTracker.Domain/Entities/Category.cs
@@ -3,6 +3,11 @@ using ExpensesTracker.Domain.Entities.Base;
 
 namespace ExpensesTracker.Domain.Entities;
 
+/// <summary>
+/// Category to be used to group <see cref="Expenses"/>.
+/// These act like enums, but are stored on the database so they can be dynamically updated throughout the application.
+/// The default permissions needed to add a category to the database are administrator permissions.
+/// </summary>
 public sealed class Category : Entity
 {
     public string Name { get; set; } = string.Empty;
@@ -18,8 +23,5 @@ public sealed class Category : Entity
 
     }
 
-    public static Category Create(string name)
-    {
-        return new Category(name);
-    }
+    public static Category Create(string name) => new(name);
 }
