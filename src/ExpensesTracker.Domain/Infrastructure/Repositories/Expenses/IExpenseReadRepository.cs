b4cd696e751f6fdc00dae81b4add79e995468e02
Antunes <36316038+zpdh@users.noreply.github.com>
2025-01-04 22:35:51 -0300

    feat: add method to query for expenses owned by a user

--- src/ExpensesTracker.Domain/Infrastructure/Repositories/Expenses/IExpenseReadRepository.cs
+++ src/ExpensesTracker.Domain/Infrastructure/Repositories/Expenses/IExpenseReadRepository.cs
@@ -1,5 +1,8 @@
-﻿namespace ExpensesTracker.Domain.Infrastructure.Repositories.Expenses;
+﻿using ExpensesTracker.Domain.Entities;
+
+namespace ExpensesTracker.Domain.Infrastructure.Repositories.Expenses;
 
 public interface IExpenseReadRepository
 {
+    Task<List<Expense>> GetExpensesByUserId(int userId);
 }
--- src/ExpensesTracker.Infrastructure/Repositories/ExpenseRepository.cs
+++ src/ExpensesTracker.Infrastructure/Repositories/ExpenseRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data;
+using Dapper;
 using ExpensesTracker.Domain.Entities;
 using ExpensesTracker.Domain.Infrastructure.Repositories.Expenses;
 using ExpensesTracker.Infrastructure.Data;
@@ -10,6 +11,17 @@ public class ExpenseRepository : IExpenseReadRepository, IExpenseWriteRepository
 {
     private readonly IDbConnection _connection;
     private readonly DataContext _context;
+
+
+    public async Task<List<Expense>> GetExpensesByUserId(int userId)
+    {
+        const string query = "SELECT * FROM Expenses WHERE UserId = @userId";
+        var parameters = new { UserId = userId };
+
+        var expenses = await _connection.QueryAsync<Expense>(query, parameters);
+
+        return expenses.ToList();
+    }
 
     public ExpenseRepository(DataContext context)
     {
